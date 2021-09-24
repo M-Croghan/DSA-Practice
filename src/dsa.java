62fd70bc191e05938091810706dc3926ea02c46d
Michael Croghan <mcrog.7@gmail.com>
2021-09-23 21:26:40 -0400

    Initial Commit

---
+++ src/dsa.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class dsa {
+
+    public static void main(String[] args) {
+        int[] arr = {43, 22, -10, 1, -90, 600, 12};
+
+        Sorter.bubbleSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+}

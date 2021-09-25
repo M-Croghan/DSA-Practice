b0c69865312d798841eea987cf6d8cdcb58d6b0b
Michael Croghan <mcrog.7@gmail.com>
2021-09-24 21:38:21 -0400

    Completed Elementary Sorting Algos

--- src/dsa.java
+++ src/dsa.java
@@ -3,10 +3,13 @@ import java.util.Arrays;
 public class dsa {
 
     public static void main(String[] args) {
-        int[] arr = {43, 22, -10, 1, -90, 600, 12};
 
-        Sorter.bubbleSort(arr);
+        int[] arr = {0, 99, -12, 43, 87, 122, -13, 8, 49, 82, 23};
         System.out.println(Arrays.toString(arr));
+        ElementarySorts.insertionSort(arr);
+        System.out.println(Arrays.toString(arr));
+
+
     }
 
 }

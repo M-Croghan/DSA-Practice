a3b880d73cafe9654677174444cbaf97ad63feb8
Michael Croghan <mcrog.7@gmail.com>
2021-09-27 21:37:05 -0400

    Data Structures, ADTs, & Big O

--- src/dsa.java
+++ src/dsa.java
@@ -1,13 +1,8 @@
-import java.util.Arrays;
+
 
 public class dsa {
 
     public static void main(String[] args) {
-
-        int[] arr = {0, 99, -12, 43, 87, 122, -13, 8, 49, 82, 23};
-        System.out.println(Arrays.toString(arr));
-        ElementarySorts.insertionSort(arr);
-        System.out.println(Arrays.toString(arr));
 
 
     }
